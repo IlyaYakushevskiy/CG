342e726d9ecdf18a68f6da5847adc895c46c627e
ILYA <ilyayakushevsky@gmail.com>
2026-03-20 23:46:29 +0100

    done HW2

---
+++ Exercise2/build/CMakeFiles/Exercise2.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/Camera.cpp" "CMakeFiles/Exercise2.dir/src/Camera.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/Camera.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/Cube.cpp" "CMakeFiles/Exercise2.dir/src/Cube.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/Cube.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/GLApp.cpp" "CMakeFiles/Exercise2.dir/src/GLApp.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/GLApp.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/GLExercise.cpp" "CMakeFiles/Exercise2.dir/src/GLExercise.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/GLExercise.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/MultiLine.cpp" "CMakeFiles/Exercise2.dir/src/MultiLine.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/MultiLine.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/ShaderProgram.cpp" "CMakeFiles/Exercise2.dir/src/ShaderProgram.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/ShaderProgram.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/Shape.cpp" "CMakeFiles/Exercise2.dir/src/Shape.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/Shape.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/Torus.cpp" "CMakeFiles/Exercise2.dir/src/Torus.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/Torus.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/UnitCube.cpp" "CMakeFiles/Exercise2.dir/src/UnitCube.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/UnitCube.cpp.o.d"
+  "/Users/ilyayakushevskiy/Desktop/CG/Exercise2/src/main.cpp" "CMakeFiles/Exercise2.dir/src/main.cpp.o" "gcc" "CMakeFiles/Exercise2.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Exercise2/build/CMakeFiles/Exercise2.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise2.dir/src/Camera.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/Cube.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/Cube.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/GLApp.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/GLApp.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/GLExercise.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/GLExercise.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/MultiLine.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/MultiLine.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/ShaderProgram.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/ShaderProgram.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/Shape.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/Shape.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/Torus.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/Torus.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/UnitCube.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/UnitCube.cpp.o.d"
+  "CMakeFiles/Exercise2.dir/src/main.cpp.o"
+  "CMakeFiles/Exercise2.dir/src/main.cpp.o.d"
+  "Exercise2"
+  "Exercise2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exercise2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
